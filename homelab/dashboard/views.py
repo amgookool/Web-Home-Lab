0610cefe3061b156da58de852b98e24ba576df41
Adrian Gookool <amgookool@hotmail.com>
2024-07-24 13:54:08 -0400

    Created the dashboard app - Added the User model

--- homelab/dashboard/views.py
+++ homelab/dashboard/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the dashboard index.")
+def index(request: HttpRequest):
+    context: dict = {
+        "name": "Adrian Gookool",
+        "age": 26
+    }
+    return render(request, "dashboard/index.html", context)
 

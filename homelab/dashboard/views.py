eaa54140ad8fb5bb8db079f0eda1c233d1d91ef7
Adrian Gookool <amgookool@hotmail.com>
2024-07-25 00:31:29 -0400

    updated dashboard app to include a login

--- homelab/dashboard/views.py
+++ homelab/dashboard/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseRedirect,
+    HttpResponseNotFound,
+)
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request: HttpRequest):
-    context: dict = {
-        "name": "Adrian Gookool",
-        "age": 26
-    }
+    context: dict = {"name": "Adrian Gookool", "age": 26}
     return render(request, "dashboard/index.html", context)
 
 
-
 def login(request: HttpRequest):
-    context: dict = {
-        "name": "Adrian Gookool",
-        "age": 26
-    }
-    return render(request, "dashboard/login.html", context)
+    if request.method == "GET":
+        context: dict = {"name": "Adrian Gookool", "age": 26}
+        return render(request, "dashboard/login.html", context)
+    if request.method == "POST":
+        postData = request.POST
+        username = postData.get("username")
+        password = postData.get("password")
+        if username == "admin" and password == "P@ssword123!":
+            return HttpResponseRedirect(reverse("index"))
+        return HttpResponseNotFound("Invalid credentials")
